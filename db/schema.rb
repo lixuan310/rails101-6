b74b48f03a1517314214f168df40678e1f54535f
lixuan310 <lixuan19900214@gmail.com>
2017-04-02 16:01:46 +0800

    show movie's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402074342) do
+ActiveRecord::Schema.define(version: 20170402075416) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
