f4cf940df7fcbb44d09f0947d82793db6289f222
lixuan310 <lixuan19900214@gmail.com>
2017-04-02 18:31:07 +0800

    implement group relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :name, presence: true
+
+  has_many :movie_relationships
+  has_many :members, through: :movie_relationships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402082024) do
+ActiveRecord::Schema.define(version: 20170402093931) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
+    t.integer  "movie_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_relationships", force: :cascade do |t|
     t.integer  "movie_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
