2b5dbf72b41a33449eb77c7ce523f44028608d68
lixuan310 <lixuan19900214@gmail.com>
2017-04-02 17:37:52 +0800

    implement comments pagination with will_paginate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
-    @comments = @movie.comments.recent
+    @comments = @movie.comments.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def edit
